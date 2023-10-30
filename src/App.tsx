7b78db1daeeeb3ea8a269b2994e3bec9474c0a8e
Shaarang T <shaarang.1910@gmail.com>
2023-10-30 22:15:28 +1100

    commentout  context

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import "./App.css";
 import "flowbite";
 import { AppRoutes } from "./routes/Routes";
 import { CalendarProvider } from "./context/calendar/CalendarContex";
-import { AnnotationProvider } from "./context/annotation/AnnotationContext";
+// import { AnnotationProvider } from "./context/annotation/AnnotationContext";
 
 function App() {
   return (
     <div class="container lg:max-w-full mx-auto">
       <CalendarProvider>
-        <AnnotationProvider>
-          <AppRoutes />
-        </AnnotationProvider>
+        {/* <AnnotationProvider> */}
+        <AppRoutes />
+        {/* </AnnotationProvider> */}
       </CalendarProvider>
     </div>
   );
